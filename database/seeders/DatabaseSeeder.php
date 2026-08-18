7d131818626264c4f3b9d6209c1dff5927d7f252
ALBaraa R. ALSaiqali <albaraawork3@gmail.com>
2026-08-18 15:23:36 +0300

    refactor(seeder): uncomment and clean up database seeder logic for clarity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,44 +22,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //     $admin = User::factory()->admin()->create([
-        //         'name' => 'Admin',
-        //         'email' => 'admin@example.com',
-        //         'password' => bcrypt('password'),
-        //     ]);
+        $admin = User::factory()->admin()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        //     $users = User::factory()->count(2)->create();
+        $users = User::factory()->count(2)->create();
 
-        //     Post::factory()->count(15)->create([
-        //         'author_id' => fn() => collect([$admin, ...$users])->random()->id,
-        //     ]);
+        Post::factory()->count(15)->create([
+            'author_id' => fn() => collect([$admin, ...$users])->random()->id,
+        ]);
 
-        //     Form::factory()->count(4)->create()->each(function (Form $form) {
-        //         $fields = collect([
-        //             FormField::factory()->for($form)->create(['order' => 1]),
-        //             FormField::factory()->email(2)->for($form)->create(),
-        //             FormField::factory()->university(3)->for($form)->create(),
-        //             FormField::factory()->attendedBefore(4)->for($form)->create(),
-        //         ]);
+        Form::factory()->count(4)->create()->each(function (Form $form) {
+            $fields = collect([
+                FormField::factory()->for($form)->create(['order' => 1]),
+                FormField::factory()->email(2)->for($form)->create(),
+                FormField::factory()->university(3)->for($form)->create(),
+                FormField::factory()->attendedBefore(4)->for($form)->create(),
+            ]);
 
-        //         FormSubmission::factory()
-        //             ->count(fake()->numberBetween(5, 25))
-        //             ->for($form)
-        //             ->create()
-        //             ->each(function (FormSubmission $submission) use ($fields) {
-        //                 foreach ($fields as $field) {
-        //                     SubmissionAnswer::factory()->create([
-        //                         'submission_id' => $submission->id,
-        //                         'field_id' => $field->id,
-        //                         'answer' => match ($field->type) {
-        //                             'email' => fake()->safeEmail(),
-        //                             'select', 'radio' => fake()->randomElement($field->options),
-        //                             default => fake()->name(),
-        //                         },
-        //                     ]);
-        //                 }
-        //             });
-        //     });
+            FormSubmission::factory()
+                ->count(fake()->numberBetween(5, 25))
+                ->for($form)
+                ->create()
+                ->each(function (FormSubmission $submission) use ($fields) {
+                    foreach ($fields as $field) {
+                        SubmissionAnswer::factory()->create([
+                            'submission_id' => $submission->id,
+                            'field_id' => $field->id,
+                            'answer' => match ($field->type) {
+                                'email' => fake()->safeEmail(),
+                                'select', 'radio' => fake()->randomElement($field->options),
+                                default => fake()->name(),
+                            },
+                        ]);
+                    }
+                });
+        });
         FieldVoice::factory()->count(10)->create();
     }
 }
